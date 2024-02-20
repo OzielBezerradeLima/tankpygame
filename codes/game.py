ad7327f9fba3cf8aa7a29d053c5c89d70ea232e2
Felipe Lopes <fal.snf23@uea.edu.br>
2024-02-19 23:44:54 -0300

    correção bug visual tank

--- codes/game.py
+++ codes/game.py
@@ -12,12 +12,14 @@ def run():
     maze.create_map()
     while playing:
         pygame.display.update()
+        maze.draw_map()
         tank.controls(pygame.time.get_ticks())
         tank.bullet_move()
         tank.bullet_player_collision()
         tank.bullet_ricochet()
         tank.player_active()
         tank.draw_bullet()
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,7 +28,7 @@ def run():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        maze.draw_map()
+
 
 
 def close_screen():
