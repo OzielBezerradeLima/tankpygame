99dbd000159566327e238730cc6f91ee22f40de5
Oziel Bezerra de Lima <obdl.snf23@uea.edu.br>
2024-02-19 22:02:04 -0400

    Atualizando game.py

--- codes/game.py
+++ codes/game.py
@@ -19,6 +19,14 @@ def run():
         tank.player_active()
         tank.draw_bullet()
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                close_screen()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
 
 
 def close_screen():
